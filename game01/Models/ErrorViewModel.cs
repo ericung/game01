d12f0bd6414611a0038611679fa0b70d8c6621f8
ericung <ungericw@tutanota.com>
2024-10-09 18:33:34 -0500

    game01 solution

--- game01/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace game01webapp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- game01/Models/HomeViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace game01webapp.Models
-{
-    public class HomeViewModel
-    {
-        public string? User { get; set; }
-    }
-}
